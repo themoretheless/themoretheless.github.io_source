4f2d88725c635943aa87850a3269ac09e6455b80
Denis Mezhov <d.o.mezhov@outlook.com>
2021-04-28 04:52:09 +0300

    dev

---
+++ src/themoretheless.github.io_source/Pages/Counter.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace themoretheless.github.io_source.Pages
+{
+    public class CounterBase : ComponentBase
+    {
+        protected int currentCount = 0;
+
+        protected void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
